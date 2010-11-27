4373d0fb1b648eb308885d701dc2ceb2f905e4ca
DAQ <daq@pcucn18.lanl.gov>
2010-11-26 21:19:47 -0700

    placed all the histogram creating and filling into one fucntion. I may split this into two functions next.

--- periodic_analyser.cc
+++ periodic_analyser.cc
@@ -35,9 +35,81 @@ class Spectrum
  * Authors: Kevin Peter Hickerson
  * Date: Oct 2010
  */
+
+Long64_t scan_time = Long64_t(5.2E9); 	// ns
+Long64_t max_time = Long64_t(520E9); 	// ns
+Long64_t start_time = Long64_t(0);      // ns
+
+int time_bin_count = 520/5;
+int area_bin_count = 32;		
+double max_area = 4096*8;               // in multiples 78 fC
+double lower_area_cut = 0;
+double upper_area_cut = max_area;
+
+float lifetime1_start_time;
+float lifetime1_stop_time;
+float lifetime2_start_time;
+float lifetime2_stop_time;
+
 void usage(const char * arg_name) 
 {
-	printf("Usage: %s <signal run number> <background run number>\n", arg_name);
+	printf("Usage: %s <signal run number> <background run number> [start time(s)] [scan time(s)] [max time(s)]\n", arg_name);
+}
+
+int make_historgram(TString filename, TString hist_name, TH2F** area_time_hist, TH1F** time_hist)
+{
+  	TFile* file = new TFile(filename);
+  	if (file->IsZombie())
+  	{
+		printf("File "+filename+"not found.\n");
+		exit(1);
+  	}
+
+  	TTree* tree = (TTree*)file->Get("allEvents");
+  	if (tree == NULL)
+  	{
+		printf("TTree not found in beta file "+filename+".\n");
+        	exit(1);
+  	}
+
+	if ( tree->GetEntries() == (long)tree->GetEntries())
+  		printf("Number of entries in the tree %li.\n", (long) tree->GetEntries());
+	else
+  		printf("Number of entries in the tree %e.\n", (double) tree->GetEntries());
+
+	*area_time_hist = new TH2F(hist_name+"_area_time_hist", "Counts per time and area", time_bin_count, 
+				   0, scan_time/1E9, area_bin_count, 0, max_area);
+	*time_hist = new TH1F(hist_name+"_time_hist", "Counts per time", 5*time_bin_count, 0, scan_time/1E9);
+	*area_hist = new TH1F(hist_name+"_area_hist", "Visable energy", area_bin_count, max_area);
+	
+	Long64_t num = 0;
+	NGammaEvent* event = new NGammaEvent();
+        tree->SetBranchAddress("evt", &event);
+	
+	int N = tree->GetEntries();
+	tree->GetEntry(0);
+	Long64_t first_time = event->peakTime;
+
+	for (int i = 0; i < N; i++)
+	{
+		if (tree->GetEntry(i) > 0)
+		{
+			Long64_t sample_time = event->peakTime - first_time;
+			if (event->channel == 16 || event->channel == 17 && sample_time < max_time)
+			{
+				double cycle_time = ((sample_time - start_time) % scan_time) / 1E9; // seconds
+				(*area_time_hist)->Fill(cycle_time, event->area);
+				if (event->area > lower_area_cut && event->area < upper_area_cut)
+					(*time_hist)->Fill(cycle_time);
+				num++;
+			}
+		}
+		else
+			std::cout << "error getting entry" << i << std::endl;
+	}
+
+	printf("Number of entries filled is %d.\n", num);
+	return num;
 }
 
 int main (int arg_c, char **arg_v)
@@ -68,21 +140,34 @@ int main (int arg_c, char **arg_v)
   	}
 */
 
-	Long64_t scan_time = (Long64_t)5.2E9; 	// in ns
-  	Long64_t scan_start_time = (Long64_t)0; // in ns
-  	Long64_t max_time = (Long64_t)520E9; 	// in ns
+	scan_time = Long64_t(5.2E9); 	// ns
+	start_time = Long64_t(0);      // ns
+	max_time = Long64_t(520E9); 	// ns
 
-	int time_bin_count = 520;
-	int area_bin_count = 32;
-	double max_area = 4096*8;
+	time_bin_count = 520/10;
+	area_bin_count = 32;		
+	max_area = 4096*8;               // in multiples 78 fC
+	lower_area_cut = 0;
+	upper_area_cut = max_area;
+
+	lifetime1_start_time = 2.5;
+	lifetime1_stop_time = 5.5;
+	lifetime2_start_time = 7;
+	lifetime2_stop_time = 25;
+
+  	if (arg_c > 3) 
+  	{
+		start_time = Long64_t(atof(arg_v[3]) * 1E9);
+	}
 
   	if (arg_c > 4) 
   	{
-		scan_start_time = Long64_t(atof(arg_v[2]) * 1E9);
+		scan_time = Long64_t(atof(arg_v[4]) * 1E9);
 	}
+	
   	if (arg_c > 5) 
   	{
-		scan_time = Long64_t(atof(arg_v[3]) * 1E9);
+		max_time = Long64_t(atof(arg_v[5]) * 1E9);
 	}
 	
   	// Construct run filename
@@ -93,18 +178,14 @@ int main (int arg_c, char **arg_v)
   	//char background_filename[1024];
   	//sprintf(background_filename, "/home/kevinh/Data/UCN/UCNb2010/processed/run%s_2D.root", arg_v[2]);
   	//sprintf(background_filename, "/home/kevinh/Data/UCN/UCNb2010/processed/run%s_2D.root", arg_v[2]);
- 
 	TString root_data_dir(getenv("UCNb_PROCESSED_DATA_DIR"));
 	//TString beta_filename(root_data_dir + "run" + arg_v[1] + "_2D.root");
 	TString beta_filename(root_data_dir + arg_v[1]);
 	//TString back_filename(root_data_dir + "run" + arg_v[2] + "_2D.root");
 	TString back_filename(root_data_dir + arg_v[2]);
 
-  	// Plot options
-  	gStyle->SetOptStat(1);
-  	gStyle->SetOptFit(1);
-
   	// Open beta ntuple
+/*
   	TFile* beta_file = new TFile(beta_filename);
   	if (beta_file->IsZombie())
   	{
@@ -118,7 +199,6 @@ int main (int arg_c, char **arg_v)
 		printf("TTree not found in beta file "+beta_filename+".\n");
         	exit(0);
   	}
-
   	// Open background ntuple
   	TFile* back_file = new TFile(back_filename);
   	if (back_file->IsZombie())
@@ -134,6 +214,7 @@ int main (int arg_c, char **arg_v)
         	exit(0);
   	}
 
+*/
   	// Define cuts
 	/*
   	TH2F* his2D[NUM_PMTS];
@@ -143,85 +224,96 @@ int main (int arg_c, char **arg_v)
   	TGraphErrors* resg[NUM_PMTS];
 	*/
 
+  	// Plot options
+  	gStyle->SetOptStat(1);
+  	gStyle->SetOptFit(1);
   	gStyle->SetPalette(1);
   	gStyle->SetOptStat("");
-
+/*
 	if ( beta_tree->GetEntries() == (long)beta_tree->GetEntries())
   		printf("Number of entries in the tree.%li\n", (long) beta_tree->GetEntries());
 	else
   		printf("Number of entries in the tree.%e\n", (double) beta_tree->GetEntries());
-
+*/
 	// This will be in a loop soon -->
 	//TString beta_hist_name("beta_spectrum_hist");
+/*
 	const char * beta_hist_name = "beta_spectrum_hist";
-	//TString back_hist_name("back_spectrum_hist");
-	//TString diff_hist_name("diff_spectrum_hist");
- 	TCut *beta_cut = new TCut("(channel==16) && ((maxSample-pedestal) < 4095) && (area > 0)");
+	const char * time_hist_name = "time_hist";
+	const char * back_hist_name = "back_spectrum_hist";
+	const char * diff_hist_name = "diff_spectrum_hist";
+*/
 
-	//TString beta_draw_cmd();
-	
-	//char beta_draw_cmd[1024];
-	//sprintf(beta_draw_cmd, "area:(peakTime-(%f*TMath::Floor(peakTime/%f))) >> %s", scan_time, scan_time, beta_hist_name);
-	//sprintf(beta_draw_cmd, "area:peakTime >> %s", beta_hist_name);
-	//printf("%s\n", beta_draw_cmd);
-	//TString beta_draw_cmd("((peakTime-"+scan_time+"*TMath::Floor(peakTime/"+scan_time+")):area) >> "+beta_hist_name);
-	//TString back_draw_cmd("(area) >> "+back_hist_name);
-	//TString diff_draw_cmd("(area) >> "+diff_hist_name);
-	//TH1F* beta_hist = new TH1F(beta_hist_name, "UCN Events", bin_count, 0, 5.25);
-	//TH1F* back_hist = new TH1F(back_hist_name, "Background Events", bin_count, 0, max_area);
-	//TH1F* diff_hist = new TH1F(diff_hist_name, "On - Off Events", bin_count, 0, max_area);
-	TH2F* beta_v_time_hist = new TH2F(beta_hist_name, "time v. Evis", 
-					  time_bin_count, 0, scan_time, area_bin_count, 0, max_area);
+	TH2F* beta_area_time_hist;
+	TH1F* beta_time_hist;
+	TH2F* back_area_time_hist;
+	TH1F* back_time_hist;
+	TH2F* diff_area_time_hist;
+	TH1F* diff_time_hist;
+
+	start_time = Long64_t(6.6E9);
+	make_historgram(beta_filename, "beta", &beta_area_time_hist, &beta_time_hist);
+	make_historgram(beta_filename, "diff", &diff_area_time_hist, &diff_time_hist);
+	start_time = Long64_t(15.75E9);
+	make_historgram(back_filename, "back", &back_area_time_hist, &back_time_hist);
+/*	
+	TH2F* beta_area_v_time_hist = new TH2F(beta_hist_name, "time v. Evis", 
+					  time_bin_count, 0, scan_time/1E9, area_bin_count, 0, max_area);
+	TH1F* beta_time_hist = new TH1F(beta_time_hist_name, "time v. counts", 10*time_bin_count, 0, scan_time/1E9);
 	
 	Long64_t num = 0;
 	NGammaEvent* event = new NGammaEvent();
-	/*
-	double area = 0;	
-	Long64_t peakTime = 0;
-	int channel = 0;
-	//void* area_address = &area;
-	//void* peakTime_address = &peakTime;
-	//void* channel_address = &channel;
-        beta_tree->SetBranchAddress("area", &area);
-        beta_tree->SetBranchAddress("peakTime", &peakTime);
-        beta_tree->SetBranchAddress("channel", &channel);
-	if (beta_tree->GetBranch("area") == NULL)
-		std::cout << "branch is null" << std::endl;;
-	*/
         beta_tree->SetBranchAddress("evt", &event);
 	
 	int N = beta_tree->GetEntries();
 	beta_tree->GetEntry(0);
-	first_time = event->peakTime;
+	Long64_t first_time = event->peakTime;
+
 	for (int i = 0; i < N; i++)
 	{
 		if (beta_tree->GetEntry(i) > 0)
 		{
-			//std::cout << "event->channel = " << event->channel << std::endl;
-			if (event->channel == 16 && )
+			Long64_t sample_time = event->peakTime - first_time;
+			if (event->channel == 16 && sample_time < max_time)
 			{
-				beta_v_time_hist->Fill((event->peakTime - start_time) % scan_time, event->area);
+				beta_area_v_time->Fill(((sample_time - start_time) % scan_time, event->area)/1E9);
+				if (event->area > lower_area_cut && event->area < upper_area_cut)
+					beta_time_hist->Fill(((sample_time - start_time) % scan_time)/1E9);
 				num++;
-				//std::cout << event->peakTime << " " <<  event->area << std::endl;
 			}
 		}
 		else
-			std::cout << "error!" << std::endl;
+			std::cout << "error getting entry" << i << std::endl;
 	}
+*/
+	diff_time_hist->Add(back_time_hist, -1.0);
+	diff_area_time_hist->Add(back_area_time_hist, -1.0);
+	
+	TF1 *lifetime1_fit = new TF1("lifetime1_fit", "[0]*exp(-x/[1])+30", lifetime1_start_time, lifetime1_stop_time);
+	TF1 *lifetime2_fit = new TF1("lifetime2_fit", "[0]*exp(-x/[1])", lifetime2_start_time, lifetime2_stop_time);
 
-	//Long64_t num = beta_tree->Draw(beta_draw_cmd, *beta_cut);
-	printf("Number of entries filled is %d.\n", num);
-	//beta_tree->Draw(diff_draw_cmd, *beta_cut);
-	//back_tree->Draw(back_draw_cmd, *beta_cut);
-	//diff_hist->Add(back_hist, -1.0);
+	lifetime1_fit->SetParameter(0,160);
+	lifetime1_fit->SetParameter(1,3);
+	//lifetime1_fit->SetParameter(2,10);
 
-	beta_v_time_hist->SetLineColor(2);
+	lifetime2_fit->SetParameter(0,40);
+	lifetime2_fit->SetParameter(1,40);
+	//lifetime2_fit->SetParameter(2,5);
+	diff_time_hist->Fit(lifetime1_fit, "R");
+	diff_time_hist->Fit(lifetime2_fit, "+R");
+
 	//back_hist->SetLineColor(4);
 	//diff_hist->SetLineColor(1);
-	//TCanvas canvas = new TCanvas("Beta spectrum and background", 1920/2,1080/2);
-	beta_v_time_hist->Draw("colz");
-	//back_hist->Draw("same");
-	//diff_hist->Draw("same");
+	TCanvas* canvas2D = new TCanvas("time_area_hist_canvas", "Beta spectrum and background", 1920/2, 1080/2);
+	diff_area_time_hist->Draw("colz");
+
+	TCanvas* canvas1D = new TCanvas("time_hist_canvas", "Time sequence", 1920/2, 1080/2);
+	beta_time_hist->SetLineColor(2);
+	back_time_hist->SetLineColor(4);
+	diff_time_hist->SetLineColor(1);
+	beta_time_hist->Draw("");
+	back_time_hist->Draw("same");
+	diff_time_hist->Draw("same");
 
     app.Run();
 
